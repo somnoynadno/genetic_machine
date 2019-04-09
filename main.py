9c7269e276bc5718cc4909b1faa58e54a616053d
somnoynadno <somnoynadno@yandex.ru>
2019-04-09 22:40:00 +0700

    Now it looks like genetic

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-from random import shuffle
-
-# TODO: upgrade this class
-class Genetic():
-	def __init__(self, executable_list):
-		self.executable_list = executable_list
-
-	def generate_shuffle(self):
-		program = self.executable_list.copy()
-		shuffle(program)
-		return program
+from random import shuffle, randint
 
 
 class Searhing_Machine():
 	def __init__(self, array, search_element):
 		self.array = array
 		self.search_element = search_element
-		self.position = 0
 
 	def execute(self, program):
 		self.exceptions = list()
 		self.warnings = list()
+
+		self.position = 0
 
 		self.program = program
 		self.pointer = 0
@@ -35,14 +26,11 @@ class Searhing_Machine():
 				self.warning(-1)
 			self.pointer += 1
 
-		print(self.exceptions)
-		print(self.warnings)
-
 		if not self.exceptions:
 			self.predict()
-			return self.answer
+			return self.answer, self.exceptions, self.warnings
 		else:
-			return None
+			return None, self.exceptions, self.warnings
 
 	# executable methods below
 	def right_shift(self):
@@ -92,50 +80,165 @@ class Searhing_Machine():
 			if no_error:
 				i += 1
 			else:
-				self.terminate(-5)
+				self.warning(-2)
 				return False
 		return True
 
 	def predict(self):
 		self.answer = self.position
-		self.terminate(0)
+		# self.terminate(0)
 
 	# exception catcher
 	def terminate(self, exit_code):
-		exception_list = ["OK", "Run-Time error", "Corrupted program", "List index out of range", "Everlasting loop", "Error in loop"]
+		exception_list = [
+		"OK", "Run-Time error",
+		"Corrupted program", "List index out of range",
+		"Everlasting loop", "Error in loop", "Array is empty"
+		]
 		self.exceptions.append(exception_list[abs(exit_code)])
 
 	# warning collector
 	def warning(self, code):
-		warning_list = ["OK", "Strange operation"]
+		warning_list = [
+		"OK", "Strange behavior",
+		"Strange behavior in loop",
+		"Pointer out of range"
+		]
 		self.warnings.append(warning_list[abs(code)])
 
-def test():
-	arr = [1, 2, 3, 4, 5, 6, 7]
-	a = 6
-	arr1 = [7, 14, 15, 11, 22, 18, 10, 1, 2, 17, 12, 21, 7, 3, 22, 1, 21, 13, 5, 18, 18, 9, 11, 22, 5, 23, 16, 9, 18, 7, 15, 19, 1, 4, 5, 2, 6, 10, 8, 7]
-	a1 = 5
-	arr2 = [27, 21, 22, 8, 19, 16, 12, 17, 19, 8, 30, 18, 25, 25, 14, 14, 18, 24, 28, 26, 25, 11, 17, 21, 30, 10, 25, 15, 30, 14, 17, 20, 13, 7, 11, 10, 9, 10, 16, 21]
-	a2 = 25
-	machine = Searhing_Machine(arr, a)
-	test_machine_1 = Searhing_Machine(arr1, a1)
-	test_machine_2 = Searhing_Machine(arr2, a2)
+def test1(program):
+	arr = [1]
+	a = 1
+	test1 = Searhing_Machine(arr, a)
+	ans, e, w = test1.execute(program)
+	if ans == None:
+		return False
+	if arr[ans] == a:
+		return True
+	return False
 
-	executable_list = ["right_shift", "left_shift", "compare_equal", "while_loop", "compare_not_equal"]
-	special = ["predict"]
+def test2(program):
+	arr = [-3, 0, 0, -2, 4, 3]
+	a = 3
+	test1 = Searhing_Machine(arr, a)
+	ans, e, w = test1.execute(program)
+	if ans == None:
+		return False
+	if arr[ans] == a:
+		return True
+	return False
 
-	generator = Genetic(executable_list)
-	program = generator.generate_shuffle()
-	ans = machine.execute(program)
-	print(ans, program)
+# TODO: upgrade this class
+class Genetic():
+	def __init__(self, executable_list):
+		self.executable_list = executable_list
+		self.answer = list()
 
-	# fine usage
+	def run(self, epoh, training_set):
+		WA_num = 8
+		fatal_num = 20
+		tier_WA = list()
+		tier_fatal = list()
 
-	"""
-	program = ["while_loop", "compare_not_equal", "right_shift", "predict"]
-	ans = machine.execute(program)
+		arr = [7, 14, 15, 11, 22, 18, 10, 1, 2, 17,
+		12, 21, 7, 3, 22, 1, 21, 13, 5, 18, 18, 9,
+		11, 22, 5, 23, 16, 9, 18, 7, 15, 19, 1, 4, 5]
+		a = 21
+
+		machine = Searhing_Machine(arr, a)
+
+		# fitness function
+		w_average = 0
+
+		# first colony
+		while True:
+			program = self.generate_random_program()
+			ans, e, w = machine.execute(program)
+			if ans != None:
+				if arr[ans] == a:
+					if test1(program):
+						if test2(program):
+							self.answer.append(program)
+				if len(tier_WA) < WA_num:
+					w_average += len(w)
+					tier_WA.append(program)
+			else:
+				if len(tier_fatal) < fatal_num:
+					tier_fatal.append(program)
+			if len(tier_fatal) == fatal_num and len(tier_WA) == WA_num:
+				break
+
+		# taking mean
+		w_average /= WA_num
+
+		# main training
+		for __ in range(epoh//800):
+			# trying to mofify WA_tier members
+			# and if their fitness better than average
+			# put them back to WA_tier
+			for i, program in enumerate(tier_WA):
+				if len(program) > 3:
+					program1 = self.reduce(program)
+					ans, e, w = machine.execute(program1)
+					if ans != None:
+						if test1(program1):
+							if test2(program1):
+								self.answer.append(program1)
+						if len(w) < w_average:
+							tier_WA[i] = program2
+
+				program2 = self.mutation(program)
+				ans, e, w = machine.execute(program2)
+				if ans != None:
+					if test1(program2):
+						if test2(program2):
+							self.answer.append(program2)
+					if len(w) < w_average:
+						tier_WA[i] = program2
+			# TODO: modify fatal_tier
+
+	def generate_shuffle(self):
+		program = self.executable_list.copy()
+		shuffle(program)
+		return program
+
+	def generate_random_program(self):
+		l = randint(3, 12)
+		program = [self.executable_list[randint(0, len(self.executable_list)-1)] for i in range(l)]
+		return program
+
+	def mutation(self, arr):
+		arr[randint(0, len(arr)-1)] = self.executable_list[randint(0, len(self.executable_list)-1)]
+		return arr
+
+	def reduce(self, arr):
+		arr.pop(randint(0, len(arr)-1))
+		return arr
+
+	def random_swap(self, arr):
+		i1, i2 = 0, 0
+		while i1 == i2:
+		    i1, i2 = randint(0, len(arr)-1), randint(0, len(arr)-1)
+		arr[i1], arr[i2] = arr[i2], arr[i1]
+		return arr
+
+	def predict(self):
+		return self.answer
+
+
+def main():
+	executable_list = [
+	"right_shift", "left_shift",
+	"compare_equal", "while_loop",
+	"compare_not_equal"
+	]
+
+	g = Genetic(executable_list)
+
+	epoh = 800
+	g.run(epoh)
+	ans = g.predict()
 	print(ans)
-	"""
 
 if __name__ == "__main__":
-	test()
+	main()
